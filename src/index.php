d1c761a08290ad09132bf3b17873fb7b141f3ad4
Zareef Ahmed <zareef@zareef.com>
2021-01-16 22:10:09 +0530

    Added phpinfo function

--- src/index.php
+++ src/index.php
@@ -30,6 +30,9 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
+<?php
+phpinfo();
+?>
     </body>
 
 </html>
